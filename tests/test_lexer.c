01062b7c53974daec34d0fff89f294c5fccb0dc1
Lyes BOURENNANI <lyes.bourennani@epita.fr>
2024-01-10 19:05:05 +0100

    test(lexer): add some tests

--- tests/test_lexer.c
+++ tests/test_lexer.c
@@ -1,10 +1,34 @@
 #include <criterion/criterion.h>
 
-#include <../src/lexer/lexer.h>
+#include "../src/lexer/lexer.h"
 
 Test(lexer, empty_string)
 {
     struct lexer *lexer = create_lexer("");
     enum token token = lexer_peek(lexer);
-    cr_assert(token == TOKEN_EOF);
+
+    cr_expect_eq(token, TOKEN_EOF,
+                 "Expected: %s. Got: %s. Token: %ld. Pos: %ld",
+                 token_string(TOKEN_EOF), token_string(token), 0, lexer->pos);
+
+    free_lexer(lexer);
+}
+
+Test(lexer, simple_if)
+{
+    struct lexer *lexer = create_lexer("if true ; then true ; else false ; fi");
+    enum token correct[] = { TOKEN_IF,   TOKEN_WORD, TOKEN_SEMICOLON,
+                             TOKEN_THEN, TOKEN_WORD, TOKEN_SEMICOLON,
+                             TOKEN_ELSE, TOKEN_WORD, TOKEN_SEMICOLON,
+                             TOKEN_FI,   TOKEN_EOF };
+
+    for (size_t i = 0; i < 11; i++)
+    {
+        enum token token = lexer_pop(lexer);
+        cr_expect_eq(
+            token, correct[i], "Expected: %s. Got: %s. Token: %ld. Pos: %ld",
+            token_string(correct[i]), token_string(token), i, lexer->pos);
+    }
+
+    free_lexer(lexer);
 }
