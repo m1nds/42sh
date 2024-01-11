c0cc6eab9e50217db3a16e56fafa42ce6d707d66
Loic Battesti <loic.battesti@epita.fr>
2024-01-11 20:23:48 +0100

    feat: now the lexer is in the main.c

--- src/main.c
+++ src/main.c
@@ -1,4 +1,21 @@
-int main(void)
+#define _POSIX_C_SOURCE 200809L
+#include <stdio.h>
+#include <string.h>
+
+#include "input/input.h"
+#include "lexer/lexer.h"
+
+int main(int argc, char **argv)
 {
-    return 0;
+    int options = 0;
+    FILE *input = parse_input(argc, argv, &options);
+    struct lexer *lexer = create_lexer(input);
+    enum token token = TOKEN_IF;
+    while (token != TOKEN_EOF)
+    {
+        printf("%s ", get_token_string(lexer));
+        token = lexer_pop(lexer);
+    }
+    printf("\n");
+    free_lexer(lexer);
 }
