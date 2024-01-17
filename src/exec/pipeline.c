9c71a7496ead0ceebc821cc738faa0be5fd68fb2
Lyes BOURENNANI <lyes.bourennani@epita.fr>
2024-01-17 20:56:23 +0100

    feat(exec): add multiple pipes

--- src/exec/pipeline.c
+++ src/exec/pipeline.c
@@ -5,54 +5,111 @@
 
 #include "exec.h"
 
-int exec_command(char **argv, int tmp_fds[2], int is_right)
+void close_fds(int **p, size_t len)
+{
+    for (size_t i = 0; i < len; i++)
+    {
+        if (p[i][0] != STDOUT_FILENO && p[i][0] != STDIN_FILENO)
+        {
+            close(p[i][0]);
+        }
+
+        if (p[i][1] != STDOUT_FILENO && p[i][1] != STDIN_FILENO)
+        {
+            close(p[i][1]);
+        }
+    }
+}
+
+int exec_command(struct ast *current, int pos, int **p, int i)
 {
     int pid = fork();
     if (pid == -1)
     {
-        exit(1);
+        return -1;
     }
     else if (!pid)
     {
-        int fd_to_rep = is_right ? 0 : 1;
-        int fd_end = is_right ? tmp_fds[0] : tmp_fds[1];
-
-        if (dup2(fd_end, fd_to_rep) == -1)
+        if (pos == 0)
         {
-            exit(1);
+            dup2(p[i][1], STDOUT_FILENO);
+        }
+        else if (pos == 1)
+        {
+            dup2(p[i - 1][0], STDIN_FILENO);
+            dup2(p[i][1], STDOUT_FILENO);
+        }
+        else
+        {
+            dup2(p[i][0], STDIN_FILENO);
         }
 
-        close(tmp_fds[0]);
-        close(tmp_fds[1]);
-
-        if (execvp(argv[0], argv) == -1)
+        if (execvp(current->value[0], current->value) == -1)
         {
-            exit(1);
+            exit(127);
         }
-
-        exit(1);
     }
 
-    return pid;
+    int out;
+    waitpid(pid, &out, 0);
+
+    return WEXITSTATUS(out);
 }
 
 int handle_pipe(struct ast *ast)
 {
-    int tmp_fds[2];
-    if (pipe(tmp_fds) == -1)
+    if (ast == NULL)
     {
         return -1;
     }
 
-    int left = exec_command(ast->children[0]->value, tmp_fds, 0);
-    int right = exec_command(ast->children[1]->value, tmp_fds, 1);
+    struct ast **current = ast->children;
+    size_t len = 0;
 
-    close(tmp_fds[0]);
-    close(tmp_fds[1]);
+    while (*current)
+    {
+        len++;
+        current++;
+    }
 
-    int out;
-    waitpid(left, &out, 0);
-    waitpid(right, &out, 0);
+    if (len <= 1)
+    {
+        return 0;
+    }
 
-    return WEXITSTATUS(out);
+    int **p = malloc((len - 1) * sizeof(int *));
+    for (size_t i = 0; i < len - 1; i++)
+        p[i] = calloc(2, sizeof(int));
+
+    for (size_t i = 0; i < len; i++)
+    {
+        if (pipe(p[i]) == -1)
+        {
+            close_fds(p, i);
+            free(p);
+            return -1;
+        }
+    }
+
+    current = ast->children;
+    exec_command(*current, 0, p, 0);
+
+    current++;
+    size_t i = 1;
+    while (*current)
+    {
+        exec_command(*current, 1, p, i);
+        i++;
+        current++;
+    }
+
+    int out = exec_command(*current, 1, p, i);
+    close_fds(p, len - 1);
+
+    for (size_t i = 0; i < len - 1; i++)
+        free(p[0]);
+
+    free(p);
+
+    return out;
 }
